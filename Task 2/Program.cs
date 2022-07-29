257ec74e4ed9d9c191e9f5203b3ae4e3a955387a
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-4PP6PL7>
2022-07-29 22:48:59 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Products[] products = JsonSerializer.Deserialize<Products[]>(jsonString);
+            Products maxProduct = products[0];
+            foreach (Products c in products)
+            {
+                if (c.costProduct > maxProduct.costProduct)
+                {
+                    maxProduct = c;
+                }
+            }
+            Console.WriteLine($"{maxProduct.codeProduct} {maxProduct.nameProduct} {maxProduct.costProduct}");
+            Console.ReadKey();
+        }
+    }
+}
